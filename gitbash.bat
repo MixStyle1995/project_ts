6a4431b803d0367122de76ac4229c6c4999663a1
Thai Son <matxichtuthan@gmail.com>
2024-09-01 22:57:48 +0700

    Update

--- gitbash.bat
+++ gitbash.bat
@@ -2,5 +2,5 @@
 git config --global user.name "Thai Son"
 git config --global user.email "matxichtuthan@gmail.com"
 git add .
-git commit -m "Update 2.83 Conver vs2013->2015"
+git commit -m "Update"
 git push origin master --force
